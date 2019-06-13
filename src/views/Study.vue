1a1a1d9d39b7d0e48e561930723ab361b6eb60e9
rlh299810 <2634320999@qq.com>
2019-06-13 11:52:19 +0800

    err

--- src/views/Study.vue
+++ src/views/Study.vue
@@ -1,11 +1,75 @@
 <template>
-  <div>
-    我的学习
+  <div class="studymodel">
+    <van-tabs v-model="active" class="van_tab" title-active-color="#2cc17b" color='#2cc17b'>
+      <van-tab title="我的课程">
+        <router-link>
+          <button>开始学习</button>
+        </router-link>
+      </van-tab>
+      <van-tab title="我的微专业" class="miciro">
+        <img src="//swap.stu.126.net/res/images/wap/common/newBlank.png?7b6c25677a53976b44f4d028d9dc761f">
+        <p>未学习微专业哦~</p>
+        <a class="mw-myclass-empty_btn" href="javascript:;">去发现精彩微专业</a>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  data () {
+    return {
+      acltive: 0,
+      couseelists:[],
+    }
+  },
+  created() {
+
+  },
 }
+
 </script>
+
+<style lang="less"  scoped>
+    body{
+        background: #f2f4f7
+    }
+    .miciro{
+        height: 280px;
+
+        width: 100%;
+    }
+
+       .miciro img{
+        margin: 0 auto;
+        padding: 0;
+        border: 0;
+        text-align: center;
+        width: 120px;
+        display: block;
+        margin-top: 40px;
+    }
+
+    .miciro a{
+        display: block;
+        height: 40px;
+        width: 200px;
+        border-radius: 5px;
+        line-height: 36px;
+        text-align: center;
+        font-size: 15px;
+        color: #3c4a55;
+        background: #2cc17b;
+        margin: 0 auto;
+        color: #fff;
+        text-decoration: none;
+    }
+    .miciro p{
+        font-size: 14px;
+        color: #99a4bf;
+        margin-top: 20px;
+        margin-bottom: 30px;
+        text-align: center;
+    }
+</style>
